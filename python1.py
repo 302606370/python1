052bbda0f578d68f10900a03b36bb7850169cc50
302606370 <44249249+302606370@users.noreply.github.com>
2019-07-09 11:00:21 +0800

    Initial commit

---
+++ python1.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# print("刘广鑫")
+# u = float(5)
+# print(u)
+# == == == == == 我的名片 == == == == ==
+# 姓名: itheima
+# QQ: xxxxxxx
+# 手机号: 185
+# xxxxxx
+# 公司地址: 北京市xxxx
+# == == == == == == == == == == == == == =
+# print("== == == == == 我的名片 == == == == ==")
+# name = "liu\n"
+# qq = "302606\n"
+# teleple = 1888888
+# print("名字:%s qq:%s 手机号:%s" % (name, qq, teleple))
+# print(" == == == == == == == == == == == == == =")
+
+
+# pwasword = input("请输入密码:")
+# print("刚刚输入密码是:%s" % pwasword)
+
